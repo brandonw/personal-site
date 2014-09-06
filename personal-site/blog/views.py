7a50cf601d35ead4780f01e16a73a9dc90cec570
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2014-09-06 11:40:18 -0400

    Add previous and next links
    In each post, link to the previous and next post, ordered by publish
    date.
    
    Also, unify the usage of self.kwargs to get the url kwargs, instead
    of misunderstanding the intent of the get_context_args **kwargs.

--- personal-site/blog/views.py
+++ personal-site/blog/views.py
@@ -11,9 +11,28 @@ class BlogHomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(BlogHomeView, self).get_context_data(**kwargs)
-        context['post'] = Post.objects.order_by('-pub_date', '-pub_time').first()
+        post_qty = Post.objects.count()
+        if post_qty > 0:
+            context['post'] = Post.objects.order_by('-pub_date', '-pub_time')[0]
+        if post_qty > 1:
+            context['prev'] = Post.objects.order_by('-pub_date', '-pub_time')[1]
         context['posts'] = Post.objects.group_by_date()
         return context
+
+def get_wrapped_posts(groups, slug):
+    prev = None
+    next = None
+    hit = False
+    for group in groups:
+        for post in group.posts:
+            if hit == False and post.slug != slug:
+                next = post
+            if hit == True:
+                prev = post
+                return (prev, next)
+            if post.slug == slug:
+                hit = True
+    return (prev, next)
 
 class BlogPostView(DetailView):
     model = Post
@@ -22,7 +41,12 @@ class BlogPostView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(BlogPostView, self).get_context_data(**kwargs)
-        context['posts'] = Post.objects.group_by_date()
+        groups = Post.objects.group_by_date()
+        context['posts'] = groups
+
+        (prev, next) = get_wrapped_posts(groups, self.kwargs['slug'])
+        context['prev'] = prev
+        context['next'] = next
         return context
 
 class BlogTagView(TemplateView):
@@ -30,7 +54,7 @@ class BlogTagView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(BlogTagView, self).get_context_data(**kwargs)
-        tagslug =  kwargs['slug']
+        tagslug =  self.kwargs['slug']
         tag = Tag.objects.get(slug=tagslug)
         context['tag'] = tag.name
         context['taggedposts'] = Post.objects.filter(tags__name__exact=tag.name).distinct()
